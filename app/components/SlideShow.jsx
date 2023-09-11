9406bfcce368b9f7ca6fc4b148aac9046939f254
Zna77 <Zna77@proton.me>
2023-09-11 16:39:38 +0100

    First commit

--- app/components/SlideShow.jsx
+++ app/components/SlideShow.jsx
@@ -16,7 +16,8 @@ const Slideshow = () => {
   const arrowStyles = {
     color: "darkGray", // Customize the color here
     transform: "scale(0.7)",
-    marginLeft: "0 10px", // Customize the font size here
+    marginLeft: -10,
+    marginRight: -10, // Customize the font size here
   };
 
   useEffect(() => {
