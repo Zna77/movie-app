09c1e37a9ec32c75e44ddf84e565d9c7bff1f52f
Zna77 <Zna77@proton.me>
2023-09-20 18:59:00 +0100

    First commit

--- utils/requests.js
+++ utils/requests.js
@@ -16,25 +16,3 @@ export const fetchMovies = async () => {
     throw error;
   }
 };
-
-// const instance = axios.create({
-//   baseURL: TMDB_BASE_URL,
-//   params: {
-//     api_key: API_KEY,
-//   },
-// });
-
-// export const fetchGenres = async (genreIds) => {
-//   try {
-//     const response = await instance.get(`/genre/movie/list`);
-
-//     const genreData = genreIds.map((genreId) => {
-//       const genre = response.data.genres.find((item) => item.id === genreId);
-//       return genre ? genre.name : "Unknown";
-//     });
-//     return genreData.join(" | ");
-//   } catch (error) {
-//     console.error("Error fetching genre data:", error);
-//     return "Unknown";
-//   }
-// };
