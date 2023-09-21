b4e6cbb9366ec5cdac87b4bbaf5a06124f02c8a2
Zna77 <Zna77@proton.me>
2023-09-21 03:37:43 +0100

    First commit

--- app/components/MovieList.jsx
+++ app/components/MovieList.jsx
@@ -79,29 +79,27 @@ export default function MovieList() {
   return (
     <div className="max-w-full h-screen mt-12">
       <div
-        className={`space-y-3 sm:grid sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 mt-3`}
+        className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-3 px-3`}
       >
         {movies.map((movie) => (
           <div
             key={movie.id}
-            className="w-fit h-auto flex flex-col flex-wrap justify-center items-center text-white mx-auto"
+            className="relative overflow-hidden rounded-lg shadow-lg bg-black group transition-all ease-in-out hover:scale-105"
           >
-            <div className="w-64 cursor-pointer sm:w-48 2xl:min-w-[300px] 2xl:max-w-[500px] flex flex-col mb-12 relative overflow-hidden group">
-              <Link href={`/movie/${movie.id}`}>
-                <Image
-                  src={`${TMDB_IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`}
-                  alt={movie.title}
-                  width={300}
-                  height={300}
-                  className="rounded-3xl object-contain"
-                />
-              </Link>
-
-              <div className="absolute left-0 right-0 bottom-0 h-16 bg-black/40 backdrop-blur-md rounded-bl-3xl rounded-br-3xl bg-opacity-50 p-3 transform translate-y-full transition-all duration-300 ease-out  group-hover:translate-y-0">
-                <h1 className="text-white text-center font-semibold text-xl truncate">
-                  {movie.title}
-                </h1>
-              </div>
+            <Link href={`/movie/${movie.id}`}>
+              <Image
+                src={`${TMDB_IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`}
+                alt={movie.title}
+                width={500}
+                height={750}
+                className="object-cover"
+              />
+            </Link>
+            <div className="p-4">
+              <h2 className="text-xl font-semibold text-white truncate">
+                {movie.title}
+              </h2>
+              <p className="text-gray-300">{movie.release_date}</p>
             </div>
           </div>
         ))}
