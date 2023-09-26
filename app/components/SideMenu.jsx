c18730b7e3d529c1a882acb5763512e13ef0945d
Zna77 <Zna77@proton.me>
2023-09-26 21:26:52 +0100

    First commit

--- app/components/SideMenu.jsx
+++ app/components/SideMenu.jsx
@@ -5,15 +5,15 @@ import { FaHome, FaCompass, FaBookmark, FaUserCircle } from "react-icons/fa";
 export default function Sidebar({ isOpen }) {
   // Conditionally set the width and transform property based on the 'isOpen' prop
   const sidebarStyles = isOpen
-    ? "fixed top-0 left-0 h-screen w-0 sm:w-96 bg-gray-900 text-white z-30 transform translate-x-0 transition-transform duration-300 ease-in-out"
-    : "fixed top-0 left-0 h-screen w-0 sm:w-64 bg-gray-900 text-white z-30 transition-all transform -translate-x-full duration-300 ease-in-out";
+    ? "hidden sm:block sm:fixed sm:top-0 sm:left-0 sm:h-screen sm:w-96 sm:bg-gray-900 sm:text-white sm:z-30 sm:transform sm:translate-x-0 sm:transition-transform sm:duration-300 sm:ease-in-out"
+    : "fixed top-0 left-0 h-screen bg-gray-900 text-white z-30 transition-all transform -translate-x-full duration-300 ease-in-out";
 
   return (
     <aside className={sidebarStyles}>
       <nav className="mt-10 p-16">
         <div className="">
           <h2 className="text-xl font-bold uppercase">Menu</h2>
-          <ul className="space-y-2 p-8 text-xl font-normal font-roboto">
+          <ul className="space-y-2 p-8 text-xl font-semibold font-roboto">
             <li>
               <Link href="/">
                 <div className="flex items-center space-x-6 p-2 hover:bg-gray-800">
@@ -25,7 +25,7 @@ export default function Sidebar({ isOpen }) {
             <li>
               <Link href="/explore">
                 <div className="flex items-center space-x-6 text-lg p-2 hover:bg-gray-800">
-                  <FaCompass />
+                  <FaCompass className="text-2xl" />
                   <span>Explore</span>
                 </div>
               </Link>
@@ -33,7 +33,7 @@ export default function Sidebar({ isOpen }) {
             <li>
               <Link href="/bookmarks">
                 <div className="flex items-center space-x-6 text-lg p-2 hover:bg-gray-800">
-                  <FaBookmark />
+                  <FaBookmark className="text-2xl" />
                   <span>Bookmarks</span>
                 </div>
               </Link>
@@ -41,7 +41,7 @@ export default function Sidebar({ isOpen }) {
             <li>
               <Link href="/favorites">
                 <div className="flex items-center space-x-6 text-lg p-2 hover:bg-gray-800">
-                  <FaUserCircle />
+                  <FaUserCircle className="text-2xl" />
                   <span>Favorites</span>
                 </div>
               </Link>
